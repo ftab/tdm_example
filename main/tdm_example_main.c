1961fdd0339c80024e9fefb7ae1c4e5b003c1e26
Dennis Field <dennisf@radiosound.com>
2022-02-08 15:42:48 -0500

    Fake TDM setup for ESP32

--- main/tdm_example_main.c
+++ main/tdm_example_main.c
@@ -1,6 +1,8 @@
-/* TDM8 Example
+/* "fake" TDM Example
 
-    This example code will output 100Hz sine wave and triangle wave to 4, 8, or 16 channels in TDM mode
+    This example code will output 100Hz sine wave and triangle wave to 4, 8, or 16 channels in a "fake" TDM mode (running i2s sample rate at 2x, 4x, or 8x)
+
+    This application requires a receiving device to understand the data format and convert to the correct TDM signal
 
     This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -17,25 +19,21 @@
 #include "esp_log.h"
 #include <math.h>
 
-#if !SOC_I2S_SUPPORTS_TDM
-#error Must build for a target that supports TDM (e.g. ESP32S3)
-#else
-
 /* Define IOs and peripheral number */
 
 #define I2S_NUM         (0)
-#define I2S_MCK_IO      (GPIO_NUM_5)
-#define I2S_BCK_IO      (GPIO_NUM_6)
-#define I2S_WS_IO       (GPIO_NUM_4)
-#define I2S_DO_IO       (GPIO_NUM_7)
+#define I2S_MCK_IO      (GPIO_NUM_0)
+#define I2S_BCK_IO      (GPIO_NUM_4)
+#define I2S_WS_IO       (GPIO_NUM_5)
+#define I2S_DO_IO       (GPIO_NUM_18)
 #define I2S_DI_IO       (-1)
 
 /* Begin modifiable parameters */
 
 #define SAMPLE_RATE     (44100) // number of audio frames per second
-#define SAMPLE_WIDTH    (24)    // width in bits of each sample (16, 24, 32)
+#define SAMPLE_WIDTH    (32)    // width in bits of each sample (16, 24, 32)
 #define CHANNEL_WIDTH   (32)    // width in bits of each channel (16, 24, 32); typically "24-bit" TDM is still 32 bits wide
-#define CHANNEL_NUM     (4)     // number of channels in each frame (4, 8, 16)
+#define CHANNEL_NUM     (8)     // number of channels in each frame (4, 8, 16)
 
 #define TEST_DATA       (1)     // 0: triangle/sine wave in L/R pairs
                                 // 1: sample data is hardcoded to a specific value per channel
@@ -48,12 +46,11 @@
 #define BYTES_PER_FRAME (CHANNEL_NUM * CHANNEL_WIDTH/8)
 
 #if CHANNEL_NUM == 4
-#define CHANNEL_MASK    (I2S_TDM_ACTIVE_CH0 | I2S_TDM_ACTIVE_CH1 | I2S_TDM_ACTIVE_CH2 | I2S_TDM_ACTIVE_CH3)
+#define FAKE_SAMPLE_RATE (SAMPLE_RATE * 2)
 #elif CHANNEL_NUM == 8
-#define CHANNEL_MASK    (I2S_TDM_ACTIVE_CH0 | I2S_TDM_ACTIVE_CH1 | I2S_TDM_ACTIVE_CH2 | I2S_TDM_ACTIVE_CH3 | I2S_TDM_ACTIVE_CH4 | I2S_TDM_ACTIVE_CH5 | I2S_TDM_ACTIVE_CH6 | I2S_TDM_ACTIVE_CH7)
+#define FAKE_SAMPLE_RATE (SAMPLE_RATE * 4)
 #elif CHANNEL_NUM == 16
-#define CHANNEL_MASK    (I2S_TDM_ACTIVE_CH0 | I2S_TDM_ACTIVE_CH1 | I2S_TDM_ACTIVE_CH2 | I2S_TDM_ACTIVE_CH3 | I2S_TDM_ACTIVE_CH4 | I2S_TDM_ACTIVE_CH5 | I2S_TDM_ACTIVE_CH6 | I2S_TDM_ACTIVE_CH7 | \
-                         I2S_TDM_ACTIVE_CH8 | I2S_TDM_ACTIVE_CH9 | I2S_TDM_ACTIVE_CH10 | I2S_TDM_ACTIVE_CH11 | I2S_TDM_ACTIVE_CH12 | I2S_TDM_ACTIVE_CH13 | I2S_TDM_ACTIVE_CH14 | I2S_TDM_ACTIVE_CH15)
+#define FAKE_SAMPLE_RATE (SAMPLE_RATE * 8)
 #else
 #error Channel numbers supported: 4, 8, 16
 #endif
@@ -90,15 +87,18 @@ static void setup_channel_test_values(int bits)
         else
         {
             /* lower 8 bits are unused for 24-bit, but it's still the same 32 bits of data being written to i2s driver either way */
+            /* our fake TDM signal uses the lower 8 bits for channel number so the receiving device will know how to align the channels and produce the correct TDM signal */
             for (int j = 0; j < CHANNEL_NUM; j += 2)
             {
                 samples_data[i*CHANNEL_NUM + j] = ~(0xFFFFFFFF >> (j+1));
+                samples_data[i*CHANNEL_NUM + j] |= j;
                 samples_data[i*CHANNEL_NUM + j + 1] = ~(0xFFFFFFFF >> (j+2));
+                samples_data[i*CHANNEL_NUM + j + 1] |= j + 1;
             }
         }
     }
     ESP_LOGI(TAG, "set clock");
-    i2s_set_clk(I2S_NUM, SAMPLE_RATE, (CHANNEL_WIDTH << 16) | bits, CHANNEL_MASK);
+    i2s_set_clk(I2S_NUM, FAKE_SAMPLE_RATE, (CHANNEL_WIDTH << 16) | bits, I2S_CHANNEL_STEREO);
 
     ESP_LOGI(TAG, "write data");
     i2s_write(I2S_NUM, samples_data, FRAMES_PER_CYCLE*BYTES_PER_FRAME, &i2s_bytes_write, 100);
@@ -158,7 +158,7 @@ static void setup_triangle_sine_waves(int bits)
 
     }
     ESP_LOGI(TAG, "set clock");
-    i2s_set_clk(I2S_NUM, SAMPLE_RATE, (CHANNEL_WIDTH << 16) | bits, CHANNEL_MASK);
+    i2s_set_clk(I2S_NUM, FAKE_SAMPLE_RATE, (CHANNEL_WIDTH << 16) | bits, I2S_CHANNEL_STEREO);
 
     ESP_LOGI(TAG, "write data");
     i2s_write(I2S_NUM, samples_data, FRAMES_PER_CYCLE*BYTES_PER_FRAME, &i2s_bytes_write, 100);
@@ -170,18 +170,18 @@ void app_main(void)
 {
     i2s_config_t i2s_config = {
         .mode = I2S_MODE_MASTER | I2S_MODE_TX,
-        .sample_rate = SAMPLE_RATE,
+        .sample_rate = FAKE_SAMPLE_RATE,
         .bits_per_sample = SAMPLE_WIDTH,
         .bits_per_chan = CHANNEL_WIDTH,
-        .chan_mask = CHANNEL_MASK,
-        .channel_format = I2S_CHANNEL_FMT_MULTIPLE,
-        .communication_format = I2S_COMM_FORMAT_PCM_SHORT, // must be PCM_SHORT now, i guess?
-        .mclk_multiple = 512, // must be at least 512 so bck divider is at least 2
+//        .chan_mask = CHANNEL_MASK,
+//        .channel_format = I2S_CHANNEL_FMT_MULTIPLE,
+//        .communication_format = I2S_COMM_FORMAT_PCM_SHORT, // must be PCM_SHORT now, i guess?
+        .mclk_multiple = 512 / (FAKE_SAMPLE_RATE / SAMPLE_RATE), // force mclk to 22.5mhz
         .dma_buf_count = 3,
         .dma_buf_len = 147,
 //        .dma_desc_num = 3,
 //        .dma_frame_num = 147,
-        .use_apll = false,
+//        .use_apll = false,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1                                //Interrupt level 1
     };
     i2s_pin_config_t pin_config = {
@@ -206,5 +206,3 @@ void app_main(void)
     }
 
 }
-
-#endif
